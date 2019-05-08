fe96db52a16a80feb5e41b34225e7687f6544896
Giacomo Tontini - giacomo.tontini@studio.unibo.it <giacomo.tontini@studio.unibo.it>
2019-05-08 13:51:14 +0200

    First commit

---
+++ src/main/java/Giacomo.java
@@ -0,0 +1,17 @@
+
+
+public class Giacomo {
+	
+	private String name;
+	private String surname;	
+
+	public Giacomo(final String name, final String surname){
+		super();
+		this.name = name;
+		this.surname= surname;
+	}
+
+	public String toString(){
+		return this.name + " " + this.surname;
+	}	
+}
